bb75650e3686a5ea7dbcd76a6e5406b8d1929214
iamSahdoon <sabiques1@gmail.com>
2024-11-15 11:47:17 +0530

    started working on  Authentication - user signup - validated user input, using fire base auth handled user registration.

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -4,7 +4,45 @@ import { Link } from "react-router-dom";
 import tologin from "../images/tologin.svg";
 import register from "../images/register.svg";
 
+import { useState } from "react";
+import { auth } from "../../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export const Signup = () => {
+  const initialState = {
+    email: { required: false },
+    password: { required: false },
+  };
+  const [errors, setErrors] = useState(initialState);
+  const [accountCreated, setAccountCreated] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault(); // prevents page from reloading when we click register
+
+    // Validation check
+    if (!email || !password) {
+      setErrors({
+        email: { required: !email }, // sets email required error if email is empty
+        password: { required: !password }, // sets password required error if password is empty
+      });
+      return; // stops the function if either field is empty
+    }
+    // Clear errors if both fields are filled
+    setErrors(initialState);
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log("Account created");
+      setAccountCreated(true); // Set account created to true on success
+    } catch (error) {
+      console.log(error);
+      setAccountCreated(false); // Clear success message on error
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -27,6 +65,8 @@ export const Signup = () => {
                 name="email"
                 id="email"
                 placeholder="Email..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 className="password"
@@ -34,13 +74,22 @@ export const Signup = () => {
                 name="name"
                 id="email"
                 placeholder="Password..."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </form>
+            <div className="error-span">
+              {errors.email.required ? <span>:( Email required</span> : null}
+              {errors.password.required ? (
+                <span>:( Password required</span>
+              ) : null}
+              {accountCreated && <span>:) Player registered successfully</span>}
+            </div>
             <div className="img-container">
               <Link to="/loginSection">
                 <img className="tosignup" src={tologin} alt="siginup" />
               </Link>
-              <button type="submit">
+              <button type="submit" onClick={handleRegister}>
                 <img className="play" src={register} alt="play" />
               </button>
             </div>
