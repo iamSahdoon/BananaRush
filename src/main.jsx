2e41973e8ae710a05bdc5deef33dc77e66149654
iamSahdoon <sabiques1@gmail.com>
2024-12-01 17:14:17 +0530

    removed some commets and change title of the webpage and favicon icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Home } from "./pages/Home/Home.jsx";
-// import App from './App.jsx'
 import { LoginSignup } from "./pages/LoginSignup/LoginSignup";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -51,9 +50,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <Home /> */}
-    {/* <LoginSignup /> */}
     <RouterProvider router={router} />
   </StrictMode>
 );
